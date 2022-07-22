bbd4b121f1214783fd7c886e7d088cc2fe93f5a3
yapiko <bloomingjewels7021@gmail.com>
2022-07-22 13:59:54 +0900

    omikuji1

---
+++ src/test/java/com/example/fortune/FortuneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fortune;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
